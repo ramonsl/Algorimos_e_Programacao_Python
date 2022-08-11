7fa83e9c64750c31592683ad92b08b2a26af549d
Ramon Lummertz <ramonsl@gmail.com>
2022-08-11 18:40:56 -0300

    Exercicio

---
+++ TDE_02/Exe_03.py
@@ -0,0 +1,6 @@
+#3. Faça um algoritmo que leia um número inteiro e imprima seu antecessor e seu sucessor.
+
+numero=int(input("digite o primeiro valor\n"))
+sucessor=numero+1
+antecessor=numero-1
+print(f"{antecessor} - { numero} - {sucessor}")
---
+++ TDE_02/exe_02.py
@@ -0,0 +1,10 @@
+#Faça um algoritmo que leia dois números reais e imprima a soma e a média aritmética desses números.
+
+a=int(input("digite o primeiro valor\n"))
+b=int(input("digite o segundo valor\n"))
+
+soma=a+b
+media=soma/2
+
+print(f"Soma={soma}")
+print(f"Media={media}")
