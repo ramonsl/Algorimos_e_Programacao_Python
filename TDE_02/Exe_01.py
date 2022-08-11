c14221ca2bf54512fa4a9674c0056f4884e90d8a
Ramon Lummertz <ramonsl@gmail.com>
2022-08-11 11:39:28 -0300

    Primeiro exe

---
+++ TDE_02/Exe_01.py
@@ -0,0 +1,10 @@
+#1. Faça um algoritmo que resolva as seguintes expressões aritméticas considerando A=2, B=5 e C=10.  Mostre o resultado na tela da expressão
+a=2
+b=5
+c=10
+#forma de exibir mais simples
+print(a+b*c/a)
+#forma de exibir concatenando o valor
+print("O resultado de (a+b)*c/a é "+ str((a+b)*c/a))
+#forma de exibir formartado
+print(f"O resultado de  (a+b*c)/a é {(a+b*c)/a}")
