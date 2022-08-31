b3dcea50b00fed06c12dfd0b0ba47d70aec7b7f2
Ramon Lummertz <ramonsl@gmail.com>
2022-08-31 20:30:00 -0300

    Exemplo
    exemplo aula

---
+++ TDE_04/Exemplo_auka.py
@@ -0,0 +1,35 @@
+
+from operator import truediv
+
+
+nome="ramon"
+nome2="ramon"
+if nome != nome2:
+    print("nomes diferente")
+else:
+    print("nomes iguais")
+is_umidade=False
+umidade=60
+
+#is_umidade=True if umidade >= 50 else is_umidade=False
+if umidade>=50:
+    is_umidade=True
+else:
+    is_umidade=False
+    
+nota_ps=float(input("Digite a nota"))
+if nota_ps >=6:
+    print("Você foi aprovado\n")
+    print("Parabéns\n")
+    if nota_ps>9:
+        print("você é d+")
+elif nota_ps<6 and nota_ps>4:
+    print("Você pegou exame")
+    print("Sem pizza galera")
+else:
+    print("você não foi aprovado")
+
+print(f"sua nota é{nota_ps}")
+
+
+
---
+++ TDE_04/exemplos.py
@@ -0,0 +1,15 @@
+numero_01=9
+numero_02=9
+
+if numero_01 > numero_02:
+    print(numero_01)
+    print("Maior numero ....")
+elif numero_01==numero_02:
+    print("iguais")
+    if numero_01==10:
+        print("numero 10")
+    else:
+        print("numero diferente de 10")
+else:
+    print(numero_02)
+    print("Maior numero")
